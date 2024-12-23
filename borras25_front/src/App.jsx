53f2fcb9c2e5f821480beb3ab4b4ea76f32bc410
AleGrimaldi <grimaldialejandro5@gmail.com>
2024-12-23 20:18:31 -0300

    se corrigio dist

--- borras25_front/src/App.jsx
+++ borras25_front/src/App.jsx
@@ -3,23 +3,21 @@ import UserList from './component/UserList';
 import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/clientes' element={<div>CLIENTES</div>} />
-          <Route path='/profesor' element={<div>PROFESOR</div>} />
-          <Route path='/admin' element={<div>ADMIN</div>} />
-          <Route path='/usuarios' element={<UserList />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/clientes' element={<div>CLIENTES</div>} />
+        <Route path='/profesor' element={<div>PROFESOR</div>} />
+        <Route path='/admin' element={<div>ADMIN</div>} />
+        <Route path='/usuarios' element={<UserList />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
