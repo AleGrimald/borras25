a5a4bc7aec1b8e9ef3bcb946590b80dd290d961f
AleGrimaldi <grimaldialejandro5@gmail.com>
2024-12-23 19:40:45 -0300

    Se arreglo el path

--- borras25_front/src/App.jsx
+++ borras25_front/src/App.jsx
@@ -1,26 +1,28 @@
-import './App.css'
-import Header from './component/Header';
-import Main from './component/Main';
-import Footer from './component/Footer';
-import UserList from './component/UserList'
-import {Routes, Route} from 'react-router-dom';
+// src/App.js
+import React from 'react';
+import './App.css';
+import UserList from './components/UserList';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  
   return (
-    <div className='App'>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/cliente' element={<div>CLIENTE</div>}/>
-        <Route path='/profesor' element={<div>PROFESOR</div>}/>
-        <Route path='/admin' element={<div>ADMIN</div>}/>
-
-        <Route path='/usuarios' element={<UserList />} />
-      </Routes>
-      <Footer/>
-    </div>
-  )
+    <Router>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/clientes' element={<div>CLIENTES</div>} />
+          <Route path='/profesor' element={<div>PROFESOR</div>} />
+          <Route path='/admin' element={<div>ADMIN</div>} />
+          <Route path='/usuarios' element={<UserList />} /> {/* Aquí incluimos el UserList */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
